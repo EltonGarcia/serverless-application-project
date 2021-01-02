1afa810c064dd1dc85cf8890193562ca2845300d
Elton Santana <elton.garcia.s@gmail.com>
2021-01-01 21:45:55 -0300

    Generate upload URL

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,28 @@
 import 'source-map-support/register'
-
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS  from 'aws-sdk'
+import * as AWSXRay from 'aws-xray-sdk'
+
+const XAWS = AWSXRay.captureAWS(AWS)
+
+const s3 = new XAWS.S3({
+  signatureVersion: 'v4'
+})
+
+const bucketName = process.env.IMAGES_S3_BUCKET
+const urlExpiration = process.env.SIGNED_URL_EXPIRATION
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
-
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+  const url = s3.getSignedUrl('putObject', {
+    Bucket: bucketName,
+    Key: todoId,
+    Expires: urlExpiration
+  })
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      uploadUrl: url
+    })
+  }
 }
